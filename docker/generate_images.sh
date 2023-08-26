28aab215391cb255be83621ec4f32c86b4a72fb3
rodneyosodo <blackd0t@protonmail.com>
2023-08-27 02:58:27 +0300

    Export docker images
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

---
+++ docker/generate_images.sh
@@ -0,0 +1,14 @@
+docker images --format "{{.Repository}}:{{.Tag}}" >dockerimages.txt
+
+while read line; do
+    # Remove private images
+    if [[ $line == *"<none>"* ]] || [[ $line == *"cocos"* ]] || [[ $line == *"ghcr.io/0x6flab/"* ]] || [[ $line == *"clients"* ]]; then
+        continue
+    fi
+    echo "docker pull $line"
+
+done <dockerimages.txt >images.sh
+
+rm dockerimages.txt
+
+chmod +x images.sh
